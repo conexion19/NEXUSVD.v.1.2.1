238d51175610061320763548035dfc973f3ac811
0 <zadoronidanil@gmail.com>
2025-12-21 21:03:07 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,4 @@
-local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/zawerex/govno435345/refs/heads/main/g"))()
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/zawerex/Nex1/refs/heads/main/Library/Nexus"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
