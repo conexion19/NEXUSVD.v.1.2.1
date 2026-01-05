00429ab47398059a6646da3cd35362d48f26613b
0 <zadoronidanil@gmail.com>
2026-01-06 02:01:10 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,7 @@
 if not _G.NEXUS_LOADER_AUTH then
-    error("error")
+    error("Access denied: Script must be loaded through Nexus loader")
 end
-_G.NEXUS_LOADER_AUTH = nil 
+_G.NEXUS_LOADER_AUTH = nil
 
 local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/zawerex/govno435345/refs/heads/main/gffff"))() 
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
